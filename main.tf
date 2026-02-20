d6995b3e1f095d07bd93feb3c63e88d2cffdaee7
ctambo <ctamborino@eqt.com>
2026-02-20 13:01:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,10 @@ provider "null" {
 locals {
   jones = ["indy", "henry", "marian"]
 }
+/* a list of local variables */
+locals {
+  jones = ["indy", "henry", "marian", "katanga"]
+}
 
 
 /* The null_resource implements the standard resource lifecycle but takes no more action */
